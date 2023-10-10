268a278cf33d9b79cbf6bcf8b811ee8e60637000
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-10-10 18:26:16 +0530

    integrated extract feature into audio.py as function

--- audio.py
+++ audio.py
@@ -1,41 +1,78 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, send_file
 import os
+import wave
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['OUTPUT_FOLDER'] = 'static/output'
 
+# Here we are checking whether folder exists else we create a new one
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
 
-@app.route('/', methods=['GET', 'POST'])
+#This is the function which hides message into audio
+def hide_message(input_audio, secret_message, output_audio):
+    waveaudio = wave.open(input_audio, mode='rb')
+    frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+    secret_message = secret_message + int((len(frame_bytes)-(len(secret_message)*8*8))/8) *'#'
+    bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in secret_message])))
+    for i, bit in enumerate(bits):
+        frame_bytes[i] = (frame_bytes[i] & 254) | bit
+    frame_modified = bytes(frame_bytes)
+    with wave.open(output_audio, 'wb') as fd:
+        fd.setparams(waveaudio.getparams())
+        fd.writeframes(frame_modified)
+    waveaudio.close()
+
+
+@app.route('/')
 def index():
-    result = ""
-
-    if request.method == 'POST':
-        audio_file = request.files['audio_file']
-        secret_message = request.form['secret_message']
-
-        if audio_file and allowed_file(audio_file.filename):
-            # Process the audio file and hide the message
-            # Implement your echo hiding logic here
-            result = "Message successfully hidden in audio."
-            # Save the modified audio file
-            modified_audio_path = os.path.join(app.config['OUTPUT_FOLDER'], 'modified_audio.wav')
-            # Use your echo hiding logic to create the modified audio file
-            # For example: modify_audio(audio_file, secret_message, modified_audio_path)
-
-    return render_template('index.html', result=result)
+    return render_template('index.html')
 
 
+@app.route('/hide', methods=['POST'])
+def upload_file():
+    #Here we asks the user to input audio file and message
+    uploaded_file = request.files['audio_file']
+    secret_message = request.form['secret_message']
+
+    if uploaded_file.filename != '':
+        # Here we save the uploaded file
+        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        uploaded_file.save(audio_path)
+
+        # Here we create external path to output audio and pass the input audio , secret message and output path to hide_message function
+        output_audio_path = os.path.join(app.config['OUTPUT_FOLDER'], 'modified_audio.wav')
+        hide_message(audio_path, secret_message, output_audio_path)
+
+        # After hiding we provide a link to download the modified audio
+        return f'<h1>Message successfully hidden in audio!</h1><a href="/download">Download Modified Audio</a>'
+    else:
+        return 'No file uploaded.'
+
+#Extract function
+@app.route('/extract', methods=['POST'])
+def extract_message():
+    uploaded_file = request.files['audio_file']
+    audio_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+    uploaded_file.save(audio_path)
+
+    waveaudio = wave.open(audio_path, mode='rb')
+    frame_bytes = bytearray(list(waveaudio.readframes(waveaudio.getnframes())))
+    extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+    string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+    msg = string.split("###")[0]
+    waveaudio.close()
+
+    return f"Hidden Message is: {msg}"
+
+#Download function to download after hiding message
 @app.route('/download')
 def download():
-    # Provide a link to download the modified audio file
     modified_audio_path = os.path.join(app.config['OUTPUT_FOLDER'], 'modified_audio.wav')
     return send_file(modified_audio_path, as_attachment=True)
 
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'wav'}
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
